749c68a4eae1c085efe3d8fd83e3f440121f7ab9
karimalisahili <kasahili.20@est.ucab.edu.ve>
2024-11-03 20:27:59 -0400

    actividad 3.5

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
       theme: ThemeData(
-        // This is the theme of your application.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
@@ -30,18 +28,39 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Welcome to Flutter"),
+        title: const Text("Welcome to Flutter"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Hello World',
-              style: TextStyle(fontSize: 30),
+      body: Stack(
+        children: <Widget>[
+          // Background image
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                    'background.jpg'), // Ensure you have this image in your assets folder
+                fit: BoxFit.cover,
+              ),
             ),
-          ],
-        ),
+          ),
+          // Center content
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'Hello World',
+                  style: TextStyle(fontSize: 30, color: Colors.white),
+                ),
+                const SizedBox(height: 20),
+                const Icon(
+                  Icons.star,
+                  size: 50,
+                  color: Colors.white,
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
